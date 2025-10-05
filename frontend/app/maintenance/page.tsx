23b0fb121b488d4603148f768047004e7c002f12
Athrael Soju <athrael.soju@gmail.com>
2025-10-05 23:35:40 +0100

    refactor: simplify configuration panel layout by removing redundant scroll area wrapper

--- frontend/app/maintenance/page.tsx
+++ frontend/app/maintenance/page.tsx
@@ -65,11 +65,9 @@ export default function MaintenancePage() {
         </div>
 
         {isConfigurationView ? (
-          <ScrollArea className="flex-1 min-h-0">
-            <div className="space-y-6 pb-6 pr-2">
-              <ConfigurationPanel />
-            </div>
-          </ScrollArea>
+          <div className="flex-1 min-h-0">
+            <ConfigurationPanel />
+          </div>
         ) : (
           <ScrollArea className="flex-1 min-h-0">
             <div className="space-y-6 pb-6 pr-2">
