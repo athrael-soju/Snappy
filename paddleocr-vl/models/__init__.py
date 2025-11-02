045798b76528d99c7b1c910b12d271a9582be09b
Athrael Soju <athrael.soju@gmail.com>
2025-11-02 22:17:37 +0000

    refactor: remove PaddleOCR-VL service files and configurations

--- paddleocr-vl/models/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Models package."""
-
-from models.api_models import ErrorResponse, HealthResponse, OCRElement, OCRResponse
-
-__all__ = ["OCRElement", "OCRResponse", "HealthResponse", "ErrorResponse"]
--- paddleocr-vl/routers/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Routers package."""
-
-from routers.ocr_router import router as ocr_router
-
-__all__ = ["ocr_router"]
